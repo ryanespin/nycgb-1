c24da0be2fded79935545436e4bb1e295a75ebfa
Ryan Espin <ryanespin@gmail.com>
2023-11-28 15:19:16 -0500

    updated amount.

--- update.sh
+++ update.sh
@@ -2,10 +2,11 @@
 
 git pull
 current_amount="$(awk -F 'const currentAmountRaised = ' '{print $2}' src/pages/index.tsx)";
-current_amount=${current_amount:1}
+#current_amount=${current_amount:1}
+current_amount="$(echo $current_amount | sed 's/ //g')"
 current_amount=${current_amount::${#current_amount}-1};
 new_val="$(($1 + $current_amount))";
-sed -i '' "35s/$current_amount/ $new_val/" "src/pages/index.tsx"
+sed -i '' "35s/$current_amount/$new_val/" "src/pages/index.tsx"
 git add src/pages/index.tsx
 git commit -m "updates currentAmountRaised to $new_val"
 git push
