6ec164b532a499d655a23f56f5f9062e170e46fd
Ryan Espin <ryanespin@gmail.com>
2023-11-28 14:27:20 -0500

    changed 32 to 35.

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ current_amount="$(awk -F 'const currentAmountRaised = ' '{print $2}' src/pages/i
 current_amount=${current_amount:1}
 current_amount=${current_amount::${#current_amount}-1};
 new_val="$(($1 + $current_amount))";
-sed -i '' "32s/$current_amount/ $new_val/" "src/pages/index.tsx"
+sed -i '' "35s/$current_amount/ $new_val/" "src/pages/index.tsx"
 git add src/pages/index.tsx
 git commit -m "updates currentAmountRaised to $new_val"
 git push
