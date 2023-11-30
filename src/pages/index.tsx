051838862b17df370e7e0c071bed57cb8f3f0e78
Ryan Espin <ryanespin@gmail.com>
2023-11-29 20:53:31 -0500

    updates currentAmountRaised to 1046

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const formatter = new Intl.NumberFormat('en-US', {
   //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
 
-const currentAmountRaised = 946;
+const currentAmountRaised = 1046;
 
   return (
     <>
